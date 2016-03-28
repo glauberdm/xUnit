4f069da79e58fa6e93ec8c24ae807d03c0bdc6c7
glauberdm <glauberbcc@gmail.com>
2016-03-28 00:32:33 -0300

    Iniciando capitulo 21. summary inicial para estrutura de contagem com TestResult.

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -1,4 +1,4 @@
-from TestCase	import TestCase
+from TestCase import TestCase
 from WasRun	import WasRun
 
 class TestCaseTest(TestCase):
@@ -10,4 +10,10 @@ class TestCaseTest(TestCase):
 		test.run()
 		assert("setUp testMethod tearDown " == test.log)
 
+	def testResult(self):
+		test = WasRun("testMethod")
+		result = test.run()
+		assert("1 run, 0 failed" == result.summary())
+
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
